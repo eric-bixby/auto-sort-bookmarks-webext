e6b79c9a8ae9e8d2e77fe5a56c06f623f034a8aa
Eric Bixby <ebixby@yahoo.com>
2016-09-05 18:36:40 -0700

    Moved worker call from main to bookmark-sorter

--- data/worker.js
+++ data/worker.js
@@ -21,22 +21,62 @@ importScripts("resource://gre/modules/workers/require.js");
 let PromiseWorker = require("resource://gre/modules/workers/PromiseWorker.js");
 
 let worker = new PromiseWorker.AbstractWorker();
-worker.dispatch = function(method, args = []) {
+worker.dispatch = function (method, args = []) {
   return self[method](...args);
 };
-worker.postMessage = function(...args) {
+worker.postMessage = function (...args) {
   self.postMessage(...args);
 };
-worker.close = function() {
+worker.close = function () {
   self.close();
 };
-worker.log = function(...args) {
+worker.log = function (...args) {
   dump("Worker: " + args.join(" ") + "\n");
 };
 self.addEventListener("message", msg => worker.handleMessage(msg));
 
-// FIXME: Components not available to worker because not thread-safe (get not found exception)
+var compare = function (a, b) {
+  return a.title > b.title;
+}
 
+/**
+ * Sort the `folders`.
+ * @param {Array.<Folder>} folders The folders to sort.
+ * @returns {Array.<Folder>} The sorted folders
+ */
 function sortAll(folders) {
+  dump("Worker: sortAll::begin\n");
+  var start = new Date().getTime();
+
+  for (let folder of folders) {
+    sortFolder(folder);
+  }
+
+  var end = new Date().getTime();
+  var time = end - start;
+  dump("Worker: sortAll::end,elapsed=" + time + "ms\n");
+
   return folders;
+}
+
+/**
+ * Sort the `folder` children.
+ * @param {Folder} folder The folder to sort.
+ */
+function sortFolder(folder) {
+  let delta = 0;
+  let length;
+
+  for (let i = 0; i < folder.children.length; ++i) {
+    folder.children[i].sort(compare);
+    length = folder.children[i].length;
+    for (let j = 0; j < length; ++j) {
+      let newIndex = j + delta;
+      let oldIndex = folder.children[i][j].index;
+      folder.children[i][j].oldIndex = oldIndex || newIndex;
+      folder.children[i][j].index = newIndex;
+    }
+
+    delta += length + 1;
+  }
 }
