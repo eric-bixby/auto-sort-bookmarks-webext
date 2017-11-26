657f76ffe392afb607131119c1fe2983be065916
Eric Bixby <ebixby@yahoo.com>
2017-11-25 21:58:41 -0800

    Fixed warning

--- build_ff.sh
+++ build_ff.sh
@@ -2,17 +2,14 @@
 
 NAME=auto-sort_bookmarks-3.0
 
+echo
 echo Building...
 ./build.sh
 
 cd build
 
-# "version_name" not supported by Firefox (warning) but still works
-#grep -v version_name manifest.json | sed -e "s/\"3.0\",/\"3.0\"/" > manifest.tmp
-#mv manifest.tmp manifest.json
-
 echo
-echo Building XPI...
+echo Archiving...
 web-ext build -a ..
 
 cd ..
