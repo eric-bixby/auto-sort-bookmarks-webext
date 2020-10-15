dc0ceedcc37c0d033c9dbf9c91701141168a9642
Eric Bixby <ebixby@yahoo.com>
2020-10-14 20:41:45 -0700

    Update copyright year

--- src/content/settings.jsx
+++ src/content/settings.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2019  Boucher, Antoni <bouanto@zoho.com>
+ * Copyright (C) 2014-2020  Boucher, Antoni <bouanto@zoho.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
