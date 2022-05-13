f5695a15e9a53afc582d0aee497ca01b0776be45
Eric Bixby <ebixby@yahoo.com>
2022-05-12 22:13:32 -0700

    Remove files

--- docker-copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ASB_VER=3.4.4
-XPI_FILE=auto-sort_bookmarks-${ASB_VER}.xpi
-
-id=$(docker create asb)
-docker cp $id:/root/auto-sort-bookmarks-webext-${ASB_VER}/${XPI_FILE} - > ${XPI_FILE}
-docker rm -v $id
-
